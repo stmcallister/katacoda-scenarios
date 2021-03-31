7ae5702b0a71b3ffa6f8231036a3730574e5a02a
Scott McAllister <smcallister@pagerduty.com>
2021-03-30 17:36:03 -0700

    bg output

--- twirlingtofreedom.com/background.sh
+++ twirlingtofreedom.com/background.sh
@@ -1,5 +1,5 @@
-echo "background start"
+echo "background start" >> .backgroundstatus
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
-echo "background end"
+echo "background done" >> .backgroundstatus
