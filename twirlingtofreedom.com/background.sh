868943e4370c8e4a972373fb87950388f8721fe6
Scott McAllister <smcallister@pagerduty.com>
2021-03-31 15:26:16 -0700

    adding logging

--- twirlingtofreedom.com/background.sh
+++ twirlingtofreedom.com/background.sh
@@ -1,5 +1,5 @@
 echo "background start" >> .backgroundstatus
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - >> .backgroundstatus
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" >> .backgroundstatus
+sudo apt-get update && sudo apt-get install terraform >> .backgroundstatus
 echo "background done" >> .backgroundstatus
