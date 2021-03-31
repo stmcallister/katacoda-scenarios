ceae871d6c29dfb1d6ea56f089b31ca7df01b1b5
Scott McAllister <smcallister@pagerduty.com>
2021-03-30 17:30:06 -0700

    tf install scripts

--- twirlingtofreedom.com/background.sh
+++ twirlingtofreedom.com/background.sh
@@ -1,1 +1,5 @@
-echo "background!!"
+echo "background start"
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+echo "background end"
